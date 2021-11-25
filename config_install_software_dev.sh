7d6308f93fe9c71f5f04a73737758bcf50c8f21c
Paul Gualambo <paul.gualambo@gmail.com>
2021-11-25 14:31:06 -0500

    fix dev tools
    fix dev tools

--- config_install_software_dev.sh
+++ config_install_software_dev.sh
@@ -5,12 +5,12 @@
 sudo apt install -y git
 sudo apt install -y nc nmap
 
-# Install bash-it
-cd ~
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
-
 # centos amzn fedora 
 sudo yum install -y git
 sudo yum install -y nc nmap
 
+
+# Install bash-it
+cd ~
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
