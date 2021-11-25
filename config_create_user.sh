e9bf152c3d649a5bc82f92dba08fe907312ed798
Paul Gualambo <paul.gualambo@gmail.com>
2021-11-25 14:29:40 -0500

    fix ssh-copy-id
    fix ssh-copy-id

--- config_create_user.sh
+++ config_create_user.sh
@@ -24,5 +24,9 @@ sudo usermod -aG wheel $USER
 
 # En la maquina donde se ejecutará la IDE
 # mykey es la llave publica de la maquina donde se ejecutará la IDE
-ssh-copy-id -i ~/.ssh/mykey user@host
-ssh user@host -A
+USER_T=<user>
+HOST_T=<host>
+ssh-copy-id -i ~/.ssh/id_rsa.pub $(USER_T)@$(HOST_T)
+eval $(ssh-agent -s)
+ssh-add
+ssh $(USER_T)@$(HOST_T) -A
